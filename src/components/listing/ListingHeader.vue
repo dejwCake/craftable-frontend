a0ff307b16a7e394e7d238e2b90107d8388a8cda
David Běhal <david.behal@team.blue>
2026-03-29 22:24:46 +0200

    Add export url

--- src/components/listing/ListingHeader.vue
+++ src/components/listing/ListingHeader.vue
@@ -1,5 +1,8 @@
 <template>
     <i class="fa fa-align-justify"></i> {{ translations.listing_title }}
+    <a v-if="exportUrl" class="btn btn-primary btn-sm float-end mb-0 ms-2" :href="exportUrl" role="button">
+        <i class="fa fa-file-excel-o"></i>&nbsp; {{ translations.export_btn }}
+    </a>
     <a v-if="createUrl" class="btn btn-primary btn-spinner btn-sm float-end mb-0" :href="createUrl" role="button">
         <i class="fa fa-plus"></i>&nbsp; {{ translations.create_btn }}
     </a>
@@ -8,11 +11,13 @@
 <script setup>
 defineProps({
     createUrl: { type: String, default: '' },
+    exportUrl: { type: String, default: '' },
     translations: {
         type: Object,
         default: () => ({
             listing_title: 'Listing',
             create_btn: 'New',
+            export_btn: 'Export',
         }),
     },
 });
