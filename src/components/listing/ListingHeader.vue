a94c1903db4682043ab70ad440e60154cf75154e
David Běhal <david.behal@team.blue>
2026-03-30 15:57:58 +0200

    Fix export icon

--- src/components/listing/ListingHeader.vue
+++ src/components/listing/ListingHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <i class="fa fa-align-justify"></i> {{ translations.listing_title }}
     <a v-if="exportUrl" class="btn btn-primary btn-sm float-end mb-0 ms-2" :href="exportUrl" role="button">
-        <i class="fa fa-file-excel-o"></i>&nbsp; {{ translations.export_btn }}
+        <i class="fa fa-file-excel"></i>&nbsp; {{ translations.export_btn }}
     </a>
     <a v-if="createUrl" class="btn btn-primary btn-spinner btn-sm float-end mb-0" :href="createUrl" role="button">
         <i class="fa fa-plus"></i>&nbsp; {{ translations.create_btn }}
